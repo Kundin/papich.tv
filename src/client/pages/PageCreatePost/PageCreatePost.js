5017a382daf088cfeaa5ec9032de6ce7a6d48bb0
Yury Kundin <yury.kundin@yandex.ru>
2019-11-17 20:27:33 +0300

    Сделано ограничение по ширине всей мобильной версии, чтобы на ПК выглядело тоже юзабельно

--- src/client/pages/PageCreatePost/PageCreatePost.js
+++ src/client/pages/PageCreatePost/PageCreatePost.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { cn } from '@bem-react/classname'
 
+import { Pad } from '../../components'
 import './PageCreatePost.css'
 
 const cnPageCreatePost = cn('PageCreatePost')
 
 export function PageCreatePost() {
-  return <div className={cnPageCreatePost()}>Create Post</div>
+  return (
+    <div className={cnPageCreatePost()}>
+      <Pad padding="s">Form</Pad>
+    </div>
+  )
 }
