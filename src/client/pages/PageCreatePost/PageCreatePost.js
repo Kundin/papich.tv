6ad9c38f85d1c77a82e6bbb8abab22dc0b05052d
Yury Kundin <yury.kundin@yandex.ru>
2019-11-19 13:45:29 +0300

    После добавления поста происходит редирект на страницу этого поста и обновление кэша

--- src/client/pages/PageCreatePost/PageCreatePost.js
+++ src/client/pages/PageCreatePost/PageCreatePost.js
@@ -1,10 +1,12 @@
 import React, { useRef } from 'react'
 import { cn } from '@bem-react/classname'
 import { useMutation } from '@apollo/react-hooks'
+import { Redirect } from 'react-router-dom'
 
 import { Pad, PreloaderPage } from '../../components'
 import { Textarea, ButtonAction } from '../../UI'
 import { useMe } from '../../graphql/hooks'
+import { POSTS } from '../../graphql/queries'
 import { CREATE_POST } from '../../graphql/mutations'
 import './PageCreatePost.css'
 
@@ -16,7 +18,23 @@ export function PageCreatePost() {
     data: { me },
     loading,
   } = useMe()
-  const [createPost, { data }] = useMutation(CREATE_POST)
+  const [createPost, { data }] = useMutation(CREATE_POST, {
+    update(cache, { data: { createPost } }) {
+      const variables = { type: 'default' }
+      let { posts } = cache.readQuery({ query: POSTS, variables })
+
+      // Добавляем пост в самое начало списка
+      posts.unshift(createPost)
+
+      cache.writeQuery({
+        query: POSTS,
+        variables,
+        data: {
+          posts,
+        },
+      })
+    },
+  })
 
   // Добавить пост
   function onAddPost() {
@@ -27,6 +45,8 @@ export function PageCreatePost() {
 
   return loading ? (
     <PreloaderPage />
+  ) : data ? (
+    <Redirect to={`/post-${data.createPost.id}`} />
   ) : (
     <div className={cnPageCreatePost()}>
       <Pad title="Новая запись">
