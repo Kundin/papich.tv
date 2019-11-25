e9d03b4b7c831a696bffcc472818612d33733642
Yury Kundin <yury.kundin@yandex.ru>
2019-11-25 19:43:58 +0300

    Текст длинных комментариев теперь можно разворачивать и сворачивать

--- src/client/components/Comment/Comment.js
+++ src/client/components/Comment/Comment.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { cn } from '@bem-react/classname'
 import PropTypes from 'prop-types'
+import TextTruncate from 'react-text-truncate'
 
 import { Avatar } from '../../components'
 import { Link } from '../../UI'
@@ -17,6 +18,8 @@ Comment.propTypes = {
 }
 
 export function Comment({ className, author, text, createdAt }) {
+  const [truncate, setTruncate] = useState(true)
+
   return (
     <div className={cnComment({}, [className])}>
       <Link to={`id${author.vkId}`}>
@@ -26,7 +29,26 @@ export function Comment({ className, author, text, createdAt }) {
         <Link to={`id${author.vkId}`}>
           <div className={cnComment('Name')}>{author.fullName}</div>
         </Link>
-        <div className={cnComment('Text')}>{text}</div>
+        <div className={cnComment('Text', { truncate })}>
+          {truncate ? (
+            <TextTruncate
+              line={3}
+              text={text}
+              textTruncateChild={
+                <span className={cnComment('Show')} onClick={() => setTruncate(false)}>
+                  Развернуть
+                </span>
+              }
+            />
+          ) : (
+            <Fragment>
+              {text}
+              <div className={cnComment('Show')} onClick={() => setTruncate(true)}>
+                Свернуть текст
+              </div>
+            </Fragment>
+          )}
+        </div>
         <div className={cnComment('Date')}>{dateToString(createdAt)}</div>
       </div>
     </div>
