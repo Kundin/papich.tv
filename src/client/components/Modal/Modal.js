a964ba7bd7608b64bdd5f715f2eed467c852e4b4
Yury Kundin <yury.kundin@yandex.ru>
2019-11-25 17:45:15 +0300

    Модальное окно закрывается по клику вне его тела

--- src/client/components/Modal/Modal.js
+++ src/client/components/Modal/Modal.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { cn } from '@bem-react/classname'
 import PropTypes from 'prop-types'
 
 import { IconTimesCircleSolid } from '../../icons'
+import { useOnClickOutside } from '../../hooks'
 import './Modal.css'
 
 const cnModal = cn('Modal')
@@ -20,9 +21,14 @@ Modal.defaultProps = {
 }
 
 export function Modal({ className, title, visible, children, onClose, ...props }) {
+  const refBody = useRef()
+
+  // Закрываем модальное окно по клику вне его тела
+  useOnClickOutside(refBody, onClose)
+
   return (
     <div {...props} className={cnModal({ visible }, [className])}>
-      <div className={cnModal('Body')}>
+      <div ref={refBody} className={cnModal('Body')}>
         {/* Шапка */}
         <div className={cnModal('Header')}>
           <h2 className={cnModal('Title')}>{title}</h2>
